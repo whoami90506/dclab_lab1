b73d21db3180b78e8a029ea0a89dcd3576975c0a
whoami90506 <whoami90506@github.com>
2019-03-11 15:49:41 +0800

    final

--- src/Top.sv
+++ src/Top.sv
@@ -1,4 +1,4 @@
-module Random ( 
+/*module Random ( 
     input i_clk,
     input i_rst, 
     output [3:0] o_number
@@ -9,13 +9,14 @@ module Random (
     logic [7:0] number, n_number;
     logic [7:0] number_shift;
     logic [7:0] number_mul;
-	logic [7:0] number_ans;
-
+	 logic [7:0] number_ans;
+		
+	 assign n_number = ((number*5+3 ) %256);
     assign o_number = number[5:2];
-    assign number_shift = {number[5:0], 2'd0}; // number*4
-    assign number_mul = number_shift + number; // number*5
-    assign number_ans = number_mul + increment; //number*5 + increment
-	assign n_number = number_ans;
+    //assign number_shift = {number[5:0], 2'd0}; // number*4
+    //assign number_mul = number_shift + number; // number*5
+    //assign number_ans = number_mul + increment; //number*5 + increment
+	 //assign n_number = number_ans;
 
     always_ff @(posedge i_clk or negedge i_rst) begin
         if(~i_rst) begin
@@ -26,7 +27,7 @@ module Random (
     end
 
 endmodule
-
+*/
 module Top(
 	input i_clk,
 	input i_rst,
@@ -40,11 +41,12 @@ module Top(
 	parameter RUN_3 = 2'b11;
 
 	logic [1:0]state, next_state;
-	logic [30:0]count, next_count;
-	logic [3:0]num;
+	logic [29:0]count, next_count;
+	logic [3:0] num;
 	logic [3:0] show_num, n_show_num;
 
-	Random random(.i_clk(i_clk), .i_rst(i_rst), .o_number(num));
+	logic [31:0] random, n_random;
+	//Random random(.i_clk(i_clk), .i_rst(i_rst), .o_number(num));
 	assign o_random_out = show_num;
 
 	always_comb begin
@@ -56,10 +58,18 @@ module Top(
 				n_show_num = show_num;
 			end
 			RUN_1: begin
-				if(count == 30'd1_0000_0000) next_state = RUN_2;
-				else if(i_start == 1) next_state = IDLE;
-				else next_state = RUN_1;
-				next_count = count + 1'b1;
+				if(count == 30'd1_0000_0000)begin
+					next_state = RUN_2;
+					next_count = count + 1'b1;
+				end
+				else if(i_start == 1)begin
+					next_state = RUN_1;
+					next_count = 0;
+				end
+				else begin
+					next_state = RUN_1;
+					next_count = count + 1'b1;
+				end
 				if(count == 30'd500_0000) n_show_num = num;
 				else if(count == 30'd1000_0000) n_show_num = num;
 				else if(count == 30'd1500_0000) n_show_num = num;
@@ -84,46 +94,67 @@ module Top(
 
 			end
 			RUN_2: begin
-				if(count == 30'd2_0000_0000) next_state = RUN_3;
-				else if(i_start == 1) next_state = IDLE;
-				else next_state = RUN_2;
-				next_count = count + 1'b1;
+				if(count == 30'd2_0000_0000)begin
+					next_state = RUN_3;
+					next_count = count + 1'b1;
+				end
+				else if(i_start == 1)begin
+					next_state = RUN_1;
+					next_count = 0;
+				end
+				else begin 
+					next_state = RUN_2;
+					next_count = count + 1'b1;
+				end
 				if(count == 30'd1_1000_0000) n_show_num = num;
-				if(count == 30'd1_2000_0000) n_show_num = num;
-				if(count == 30'd1_3000_0000) n_show_num = num;
-				if(count == 30'd1_4000_0000) n_show_num = num;
-				if(count == 30'd1_5000_0000) n_show_num = num;
-				if(count == 30'd1_6000_0000) n_show_num = num;
-				if(count == 30'd1_7000_0000) n_show_num = num;
-				if(count == 30'd1_8000_0000) n_show_num = num;
-				if(count == 30'd1_9000_0000) n_show_num = num;
-				if(count == 30'd2_0000_0000) n_show_num = num;
+				else if(count == 30'd1_2000_0000) n_show_num = num;
+				else if(count == 30'd1_3000_0000) n_show_num = num;
+				else if(count == 30'd1_4000_0000) n_show_num = num;
+				else if(count == 30'd1_5000_0000) n_show_num = num;
+				else if(count == 30'd1_6000_0000) n_show_num = num;
+				else if(count == 30'd1_7000_0000) n_show_num = num;
+				else if(count == 30'd1_8000_0000) n_show_num = num;
+				else if(count == 30'd1_9000_0000) n_show_num = num;
+				else if(count == 30'd2_0000_0000) n_show_num = num;
 				else n_show_num = show_num;
 			end
 			RUN_3: begin
-				if(count == 30'd300000000) next_state = IDLE;
-				else if(i_start == 1) next_state = IDLE;
-				else next_state = RUN_3;
-				next_count = count + 1'b1;		
+				if(count == 30'd3_0000_0000)begin
+					next_state = IDLE;
+					next_count = count + 1'b1;
+				end
+				else if(i_start == 1)begin
+					next_state = RUN_1;
+					next_count = 0;
+				end
+				else begin 
+					next_state = RUN_3;
+					next_count = count + 1'b1;		
+				end
 				if(count == 30'd2_2500_0000) n_show_num = num;	
-				if(count == 30'd2_5000_0000) n_show_num = num;
-				if(count == 30'd2_7500_0000) n_show_num = num;
-				if(count == 30'd3_0000_0000) n_show_num = num;
+				else if(count == 30'd2_5000_0000) n_show_num = num;
+				else if(count == 30'd2_7500_0000) n_show_num = num;
+				else if(count == 30'd3_0000_0000) n_show_num = num;
 				else n_show_num = show_num;	
 			end
 		endcase
 	end
 
+	assign n_random = random*16807 % 2147483647;
+	assign num = random[7:4];
+	
 	always_ff@(posedge i_clk or negedge i_rst) begin
 		if(~i_rst)begin
 			state <= IDLE;
 			count <= 30'd0;
 			show_num <= 4'd0;
+			random <= 32'd1;
 		end
 		else begin
 			state <= next_state;
 			count <= next_count;
 			show_num <= n_show_num;
+			random <= n_random;
 		end
 	end
 
